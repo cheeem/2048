7b6c7f0be5605a6728eb57d2276d116daca1d9e8
cheemie <74575896+cheeem@users.noreply.github.com>
2022-12-27 01:34:05 -0500

    Delete src directory

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-//return a css variable given an n number color
-export const color = (n) => `var(--color-${n})`;
-//return a random int between a specified min and max
-export const randomIntBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-//return a random index of an array
-export const randomIndex = (arr) => Math.floor(Math.random()*arr.length);
-
